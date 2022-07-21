7421475b34e3dc7067f00ef8f5e2e7f0843b426f
Pedro Plasencia <programacion.en.esp@gmail.com>
2022-07-21 22:34:45 +0200

    initial commit with app template

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,17 +3,12 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 import { HomePageRoutingModule } from './home-routing.module';
 
-
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  providers: [InAppBrowser],
+  declarations: [HomePage],
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { App } from '@capacitor/app';
+import { Component, OnInit } from '@angular/core';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  constructor(private appBrowser: InAppBrowser) {}
+  private browserSub: Subscription;
 
-  constructor() {}
+  ngOnInit() {
+    const browser = this.appBrowser.create(
+      'https://cafeceros.com/',
+      '_self',
+      'location=no,hidenavigationbuttons=true,hideurlbar=true,zoom=no'
+    );
+    this.browserSub = browser.on('exit').subscribe((evt) => {
+      App.exitApp();
+    });
+  }
 
+  ngOnDestroy() {
+    this.browserSub.unsubscribe();
+  }
 }
