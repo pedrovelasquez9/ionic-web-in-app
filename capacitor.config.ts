99fdb473a174f8f8a7ceb40ab5e647920c59677d
Pedro Plasencia <programacion.en.esp@gmail.com>
2024-01-22 22:36:43 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'web-apk-2024',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
