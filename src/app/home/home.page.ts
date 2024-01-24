6c26db5f85eb04a9e4604e6018b4a5dd06086884
Pedro Plasencia <programacion.en.esp@gmail.com>
2024-01-24 18:21:10 +0100

    feat: update to ionic latest version

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { App } from '@capacitor/app';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,20 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  providers: [InAppBrowser]
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  constructor(private browser: InAppBrowser) { }
+
+  ngOnInit(): void {
+    const browserInstance = this.browser.create(
+      'https://cafeceros.com/',
+      '_self',
+      'location=no,hidenavigationbuttons=true,hideurlbar=true,zoom=no'
+    );
+    browserInstance.on('exit').subscribe((evt) => {
+      App.exitApp();
+    });
+  }
+
 }
